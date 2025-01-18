6c2f12a60e290b9338cc0f20dee8b6556c20994b
Vyas Ramasubramani <vyasr@nvidia.com>
2025-01-18 05:41:46 +0000

    Fix some dirs

--- ci/build_wheel.sh
+++ ci/build_wheel.sh
@@ -9,16 +9,16 @@ package_dir="python/rapids-logger"
 rapids-logger "Building '${package_name}' wheel"
 sccache --zero-stats
 python -m pip wheel \
-    -w dist \
+    -w "${package_dir}/dist" \
     -v \
     --no-deps \
     --disable-pip-version-check \
     "${package_dir}"
 sccache --show-adv-stats
 
-mkdir -p ${package_dir}/final_dist
+mkdir -p "${package_dir}/final_dist"
 python -m auditwheel repair \
-    -w ${package_dir}/final_dist \
-    ${package_dir}/dist/*
+    -w "${package_dir}/final_dist" \
+    "${package_dir}/dist/*"
 
 RAPIDS_PY_WHEEL_NAME="${package_name}" rapids-upload-wheels-to-s3 cpp "${package_dir}/final_dist"
