9b90a48b48d598ff9edc0e30f36e2520eb2c5675
Vyas Ramasubramani <vyasr@nvidia.com>
2025-01-23 04:19:41 +0000

    Improve test coverage

--- tests/basic_test.cpp
+++ tests/basic_test.cpp
@@ -26,9 +26,9 @@ struct LoggerTest : public ::testing::Test {
   LoggerTest()
     : oss{}, logger_{"logger_test", {std::make_shared<rapids_logger::ostream_sink_mt>(oss)}}
   {
+    // Remove extra formatting to simplify validation of outputs.
     logger_.set_pattern("%v");
   }
-  ~LoggerTest() override { logger_.sinks().pop_back(); }
 
   void clear_sink() { oss.str(""); }
   std::string sink_content() { return oss.str(); }
@@ -37,13 +37,13 @@ struct LoggerTest : public ::testing::Test {
   rapids_logger::logger logger_;
 };
 
-TEST_F(LoggerTest, Basic)
+TEST_F(LoggerTest, DefaultLevel)
 {
-  logger_.critical("crit msg");
-  ASSERT_EQ(this->sink_content(), "crit msg\n");
+  // The default level should not change without suitable warning to consumers.
+  EXPECT_EQ(logger_.level(), rapids_logger::level_enum::info);
 }
 
-TEST_F(LoggerTest, DefaultLevel)
+TEST_F(LoggerTest, DefaultLevelLogs)
 {
   logger_.trace("trace");
   logger_.debug("debug");
@@ -51,7 +51,18 @@ TEST_F(LoggerTest, DefaultLevel)
   logger_.warn("warn");
   logger_.error("error");
   logger_.critical("critical");
-  ASSERT_EQ(this->sink_content(), "info\nwarn\nerror\ncritical\n");
+  EXPECT_EQ(this->sink_content(), "info\nwarn\nerror\ncritical\n");
+}
+
+TEST_F(LoggerTest, DefaultLevelLogsLogFunction)
+{
+  logger_.log(rapids_logger::level_enum::trace, "trace");
+  logger_.log(rapids_logger::level_enum::debug, "debug");
+  logger_.log(rapids_logger::level_enum::info, "info");
+  logger_.log(rapids_logger::level_enum::warn, "warn");
+  logger_.log(rapids_logger::level_enum::error, "error");
+  logger_.log(rapids_logger::level_enum::critical, "critical");
+  EXPECT_EQ(this->sink_content(), "info\nwarn\nerror\ncritical\n");
 }
 
 TEST_F(LoggerTest, CustomLevel)
@@ -67,4 +78,70 @@ TEST_F(LoggerTest, CustomLevel)
   logger_.trace("trace");
   logger_.debug("debug");
   ASSERT_EQ(this->sink_content(), "debug\n");
+}
+
+std::string logged = "";
+void example_callback(int lvl, const char* msg) { logged = std::string(msg); }
+
+bool check_if_logged(rapids_logger::logger const& logger,
+                     const std::string& msg,
+                     rapids_logger::level_enum log_level_def)
+{
+  bool actually_logged  = logged.find(msg) != std::string::npos;
+  bool should_be_logged = logger.level() <= log_level_def;
+  return actually_logged == should_be_logged;
+}
+
+TEST_F(LoggerTest, TwoSinks)
+{
+  std::ostringstream oss2;
+  logger_.sinks().push_back(std::make_shared<rapids_logger::ostream_sink_mt>(oss2));
+  logger_.set_pattern("%v");
+
+  logger_.info("info");
+  EXPECT_EQ(this->sink_content(), "info\n");
+  EXPECT_EQ(oss2.str(), "info\n");
+}
+
+TEST_F(LoggerTest, CallbackSink)
+{
+  std::string msg;
+  logger_.sinks().clear();
+  logger_.sinks().push_back(std::make_shared<rapids_logger::callback_sink_mt>(example_callback));
+
+  msg = "This is a critical message";
+  logger_.critical(msg.c_str());
+  EXPECT_TRUE(check_if_logged(logger_, msg, rapids_logger::level_enum::critical));
+
+  msg = "This is an error message";
+  logger_.error(msg.c_str());
+  EXPECT_TRUE(check_if_logged(logger_, msg, rapids_logger::level_enum::error));
+
+  msg = "This is a warning message";
+  logger_.warn(msg.c_str());
+  EXPECT_TRUE(check_if_logged(logger_, msg, rapids_logger::level_enum::warn));
+
+  msg = "This is an info message";
+  logger_.info(msg.c_str());
+  EXPECT_TRUE(check_if_logged(logger_, msg, rapids_logger::level_enum::info));
+
+  msg = "This is a debug message";
+  logger_.debug(msg.c_str());
+  EXPECT_TRUE(check_if_logged(logger_, msg, rapids_logger::level_enum::debug));
+
+  msg = "This is a trace message";
+  logger_.trace(msg.c_str());
+  EXPECT_TRUE(check_if_logged(logger_, msg, rapids_logger::level_enum::trace));
+}
+
+int flush_count = 0;
+void example_flush() { ++flush_count; }
+
+TEST_F(LoggerTest, FlushTest)
+{
+  logger_.sinks().clear();
+  logger_.sinks().push_back(
+    std::make_shared<rapids_logger::callback_sink_mt>(example_callback, example_flush));
+  logger_.flush();
+  EXPECT_EQ(1, flush_count);
 }
