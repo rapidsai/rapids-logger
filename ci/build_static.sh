8da2b9c0a22bb74760fda5ae2a1fa21899a327a5
Vyas Ramasubramani <vyasr@nvidia.com>
2025-01-31 23:38:54 +0000

    Fix path

--- ci/build_static.sh
+++ ci/build_static.sh
@@ -7,6 +7,6 @@ source rapids-date-string
 
 rapids-logger "Static cpp build"
 
-cmake -S cpp -B build -DBUILD_SHARED_LIBS=OFF -DBUILD_TESTS=ON
+cmake -S . -B build -DBUILD_SHARED_LIBS=OFF -DBUILD_TESTS=ON
 cmake --build build
 ctest --test-dir build --output-on-failure
