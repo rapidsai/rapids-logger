b2e7da85bba2e6a846157a95f820db32c5c33796
Bradley Dice <bdice@bradleydice.com>
2025-02-10 11:52:59 -0600

    Simplify update-version.sh

--- ci/release/update-version.sh
+++ ci/release/update-version.sh
@@ -13,12 +13,7 @@ NEXT_FULL_TAG=$1
 # Get <major>.<minor> for next version
 NEXT_MAJOR=$(echo "$NEXT_FULL_TAG" | awk '{split($0, a, "."); print a[1]}')
 NEXT_MINOR=$(echo "$NEXT_FULL_TAG" | awk '{split($0, a, "."); print a[2]}')
-NEXT_PATCH=$(echo "$NEXT_FULL_TAG" | awk '{split($0, a, "."); print a[3]}')
 NEXT_SHORT_TAG=${NEXT_MAJOR}.${NEXT_MINOR}
-
-# Need to distutils-normalize the versions for some use cases
-NEXT_SHORT_TAG_PEP440=$(python -c "from packaging.version import Version; print(Version('${NEXT_SHORT_TAG}'))")
-PATCH_PEP440=$(python -c "from packaging.version import Version; print(Version('${NEXT_PATCH}'))")
 
 echo "Updating RAPIDS versions to $NEXT_FULL_TAG"
 
