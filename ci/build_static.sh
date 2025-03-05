e72464385b2e810b6a7ff4731bc0e41adb24ee68
Vyas Ramasubramani <vyasr@nvidia.com>
2025-03-05 02:06:16 +0000

    Activate the environment

--- ci/build_static.sh
+++ ci/build_static.sh
@@ -15,6 +15,11 @@ rapids-dependency-file-generator --file-key test_static_library --output conda -
 
 rapids-mamba-retry env create --yes -f "${ENV_FILE}" -n test
 
+# Temporarily allow unbound variables for conda activation.
+set +u
+conda activate test
+set -u
+
 cmake -S . -B build -DBUILD_SHARED_LIBS=OFF -DBUILD_TESTS=ON
 cmake --build build
 ctest --test-dir build --output-on-failure
