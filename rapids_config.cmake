8bb5662afb7da2d7b67dc7923ff8144775856a6c
Vyas Ramasubramani <vyasr@nvidia.com>
2025-09-30 00:25:16 +0000

    Add RAPIDS.cmake to enable easier overriding of what is fetched

--- rapids_config.cmake
+++ rapids_config.cmake
@@ -25,19 +25,10 @@ if(_rapids_version MATCHES [[^([0-9][0-9])\.([0-9][0-9])\.([0-9][0-9])]])
   set(RAPIDS_VERSION "${RAPIDS_VERSION_MAJOR}.${RAPIDS_VERSION_MINOR}.${RAPIDS_VERSION_PATCH}")
 else()
   string(REPLACE "\n" "\n  " _rapids_version_formatted "  ${_rapids_version}")
-  message(
-    FATAL_ERROR
-      "Could not determine RAPIDS version. Contents of VERSION file:\n${_rapids_version_formatted}"
-  )
+  message(FATAL_ERROR "Could not determine RAPIDS version. Version :\n${_rapids_version_formatted}")
 endif()
 
-if(NOT EXISTS
-   "${CMAKE_CURRENT_BINARY_DIR}/RAPIDS_LOGGER_RAPIDS-${RAPIDS_VERSION_MAJOR_MINOR}.cmake"
-)
-  file(
-    DOWNLOAD
-    "https://raw.githubusercontent.com/rapidsai/rapids-cmake/branch-${RAPIDS_VERSION_MAJOR_MINOR}/RAPIDS.cmake"
-    "${CMAKE_CURRENT_BINARY_DIR}/RAPIDS_LOGGER_RAPIDS-${RAPIDS_VERSION_MAJOR_MINOR}.cmake"
-  )
+if(NOT rapids-cmake-version)
+  set(rapids-cmake-version "${RAPIDS_VERSION_MAJOR_MINOR}")
 endif()
-include("${CMAKE_CURRENT_BINARY_DIR}/RAPIDS_LOGGER_RAPIDS-${RAPIDS_VERSION_MAJOR_MINOR}.cmake")
+include("${CMAKE_CURRENT_LIST_DIR}/cmake/RAPIDS.cmake")
