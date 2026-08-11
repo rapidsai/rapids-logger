ee954e5efb0045df8d9e8b9dfa2e8b8a9d384017
Julien Jomier <219040+jjomier@users.noreply.github.com>
2026-08-11 19:08:00 -0400

    Update RAPIDS_LOGGER_EXPORT definition for cross-platform compatibility (#62)
    Modified the RAPIDS_LOGGER_EXPORT macro to use __declspec(dllexport) for MSVC builds, ensuring proper visibility settings across different compilers.
    
    Authors:
      - Julien Jomier (https://github.com/jjomier)
      - Vyas Ramasubramani (https://github.com/vyasr)
    
    Approvers:
      - Vyas Ramasubramani (https://github.com/vyasr)
    
    URL: https://github.com/rapidsai/rapids-logger/pull/62

--- include/rapids_logger/logger.hpp
+++ include/rapids_logger/logger.hpp
@@ -1,5 +1,5 @@
 /*
- * SPDX-FileCopyrightText: Copyright (c) 2024, NVIDIA CORPORATION.
+ * SPDX-FileCopyrightText: Copyright (c) 2024-2026, NVIDIA CORPORATION & AFFILIATES. All rights reserved.
  * SPDX-License-Identifier: Apache-2.0
  */
 
@@ -13,7 +13,11 @@
 #include <type_traits>
 #include <vector>
 
+#ifdef _MSC_VER
+#define RAPIDS_LOGGER_EXPORT __declspec(dllexport)
+#else
 #define RAPIDS_LOGGER_EXPORT __attribute__((visibility("default")))
+#endif
 
 namespace rapids_logger {
 
